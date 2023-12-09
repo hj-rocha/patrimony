89aa25d7743fbbfc0b80d5dfddac22d09b8bfe44
Halisson José Soares da Rocha <hj_rocha@id.uff.br>
2023-12-09 12:43:34 -0300

    Commit inicial

---
+++ patrimony/src/main/java/br/iff/bji/patrimony/api/PatrimonyApplication.java
@@ -0,0 +1,13 @@
+package br.iff.bji.patrimony.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrimonyApplication.class, args);
+	}
+
+}
---
+++ patrimony/src/test/java/br/iff/bji/patrimony/api/PatrimonyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.iff.bji.patrimony.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
