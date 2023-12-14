d5dd1237862c62234e839e345abbfa4b23d80cc0
Yan-Balbi <yanbalbi@gmail.com>
2023-12-14 09:33:48 -0300

    Alterado o método de atualização de type of event. Agora é atualização passando id como parametro de identificacao.

--- patrimony/src/main/java/br/iff/bji/patrimony/api/service/TypeOfEventService.java
+++ patrimony/src/main/java/br/iff/bji/patrimony/api/service/TypeOfEventService.java
@@ -32,42 +32,60 @@ public class TypeOfEventService {
 		return typeOfEventRepository.findAll();
 	}
 	
-	@Transactional
-	public String updateTypeOfEvent(TypeOfEvent typeOfEvent) {
-		if(typeOfEventRepository.existsByDescription(typeOfEvent.getDescription())){
-			try {
-				List<TypeOfEvent> typesOfEvents = typeOfEventRepository.findByDescription(typeOfEvent.getDescription());
-				typesOfEvents.stream().forEach(e -> {
-					//não teria que ter outra verificação aqui não? uma verificação pra saber se o id que você quer
-					//é igual ao que você está comparando
-					TypeOfEvent typeOfEventToBeUpdate = typeOfEventRepository.findById(e.getId()).get();
-					typeOfEventToBeUpdate.setDescription(typeOfEvent.getDescription());
-					typeOfEventRepository.save(typeOfEventToBeUpdate);
-				});
-				return "Type of event updated.";
-			} catch (Exception e) {
-				throw e;
-			}
-		} else {
-			return "Type of event does not exists in the database.";
-		}
-	}
+//	@Transactional
+//	public String updateTypeOfEvent(TypeOfEvent typeOfEvent) {
+//		//if(typeOfEventRepository.existsByDescription(typeOfEvent.getDescription())){
+//			try {
+//				//List<TypeOfEvent> typesOfEvents = typeOfEventRepository.findByDescription(typeOfEvent.getDescription());
+//				//typesOfEvents.stream().forEach(e -> {
+//					//não teria que ter outra verificação aqui não? uma verificação pra saber se o id que você quer
+//					//é igual ao que você está comparando
+//					//TypeOfEvent typeOfEventToBeUpdate = typeOfEventRepository.findById(e.getId()).get();
+//					//typeOfEventToBeUpdate.setDescription(typeOfEvent.getDescription());
+//					//typeOfEventRepository.save(typeOfEventToBeUpdate);
+//				//});
+//				typeOfEventRepository.
+//				return "Type of event updated.";
+//			} catch (Exception e) {
+//				throw e;
+//			}
+////		} else {
+////			return "Type of event does not exists in the database.";
+////		}
+//	}
+	
+    @Transactional
+    public String updateTypeOfEvent(TypeOfEvent updatedTypeOfEvent) {
+        try {
+            TypeOfEvent existingTypeOfEvent = typeOfEventRepository.findById(updatedTypeOfEvent.getId()).orElse(null);
+
+            if(existingTypeOfEvent == null) {
+            	return "TypeOfEvent not found with id: " + updatedTypeOfEvent.getId();
+            }
+            
+            String updatedDescription = updatedTypeOfEvent.getDescription();
+            if (!existingTypeOfEvent.getDescription().equals(updatedDescription)
+                    && typeOfEventRepository.existsByDescription(updatedDescription)) {
+                return "Cannot update TypeOfEvent. Another TypeOfEvent with the same description already exists.";
+            }
+
+            existingTypeOfEvent.setDescription(updatedDescription);
+            typeOfEventRepository.save(existingTypeOfEvent);
+
+            return "TypeOfEvent updated successfully.";
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
 	
 	@Transactional
 	public String deleteTypeOfEvent(TypeOfEvent typeOfEvent) {
 		try {
-			//List<TypeOfEvent> typesOfEvents = typeOfEventRepository.findByDescription(typeOfEvent.getDescription());
-			//typesOfEvents.stream().forEach(s -> {
-				//não teria que ter outra verificação aqui não? uma verificação pra saber se o id que você quer
-				//é igual ao que você está comparando
-				//System.out.println(s.getDescription());
-				//typeOfEventRepository.delete(s);
 			typeOfEventRepository.deleteById(typeOfEvent.getId());
-			//});
 			return "Type of Event record deleted successfully.";
 		} catch (Exception e){
 			throw e;
 		}
-		
 	}
 }
